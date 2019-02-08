ab400b08a464b78a4c626a145fa8323091920e8d
Erislandio <erislandiosoares21@gmail.com>
2019-02-08 21:45:36 -0200

    Adição do container

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './content.css'
 
 class Content extends Component {
     render() {
@@ -6,9 +7,14 @@ class Content extends Component {
             <div id="content">
                 <div className="search">
                     <div className="modal">
+                        <span className="icon-user">
+                                <img src="https://image.flaticon.com/icons/svg/149/149072.svg" width="120px" height="120px" alt="user" />
+                        </span>
                         <label>Nome do usuário</label>
                         <input type="text" placeholder="Nome" className="input" id="input-name" />
-                        <button type="button" id="btn-search" value="Buscar"/>
+                        <button type="button" id="btn-search" value="Buscar">
+                            Buscar
+                        </button>
                     </div>
                 </div>
             </div>
